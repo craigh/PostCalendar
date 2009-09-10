29947668de8a6e96fb598c074620c09e4a0cc3a7
Craig Heydenburg <craigh@mac.com>
2009-09-10 20:43:04 +0000

    fixes #57. addresses #36

--- PostCalendar/pnlang/eng/global.php
+++ PostCalendar/pnlang/eng/global.php
@@ -1,28 +1,12 @@
 <?php
 /**
- * $Id$
- *
- * PostCalendar::PostNuke Events Calendar Module
- * Copyright (C) 2002  The PostCalendar Team
- * http://postcalendar.tv
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * To read the license please read the docs/license.txt or visit
- * http://www.gnu.org/copyleft/gpl.html
- *
+ * @package     PostCalendar
+ * @author      $Author$
+ * @link        $HeadURL$
+ * @version     $Id$
+ * @copyright   Copyright (c) 2002, The PostCalendar Team
+ * @copyright   Copyright (c) 2009, Craig Heydenburg, Sound Web Development
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License
  */
 
 //=========================================================================
@@ -192,3 +176,6 @@ define('_PC_DATE_FORMAT_INT','%A, %d %B %Y');
 define('_PC_DATE_FORMAT_STD','%A, %B %d %Y');
 define('_PC_DATE_FORMAT_BLOCK_INT','%a, %d %B %Y');
 define('_PC_DATE_FORMAT_BLOCK_STD','%a, %B %d %Y');
+
+define ('_DELETEEVENT', 'Delete Event');
+define ('_PC_EVENTREVUE', 'Event Review');
