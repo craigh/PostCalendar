4590694bde90db0357ac04d00084f38b7af2b327
Craig Heydenburg <craigh@mac.com>
2009-09-26 15:35:35 +0000

    refs #62 and #36. improved inline code documentation and corrected gettext-conversion errors.

--- PostCalendar/pnadmin.php
+++ PostCalendar/pnadmin.php
@@ -24,6 +24,11 @@ function postcalendar_admin_main()
     return postcalendar_admin_modifyconfig();
 }
 
+/**
+ * @function    postcalendar_admin_modifyconfig
+ * @description present administrator options to change module configuration
+ * @return      config template
+ */
 function postcalendar_admin_modifyconfig()
 {
     if (!pnSecAuthAction(0, 'PostCalendar::', '::', ACCESS_ADMIN)) {
@@ -35,6 +40,11 @@ function postcalendar_admin_modifyconfig()
     return $pnRender->fetch('admin/postcalendar_admin_modifyconfig.htm');
 }
 
+/**
+ * @function    postcalendar_admin_listapproved
+ * @description list all events that have been previously approved
+ * @return      list of events
+ */
 function postcalendar_admin_listapproved()
 {
     $dom = ZLanguage::getModuleDomain('PostCalendar');
@@ -45,6 +55,11 @@ function postcalendar_admin_listapproved()
     return postcalendar_admin_showlist($args);
 }
 
+/**
+ * @function    postcalendar_admin_listhidden
+ * @description list all events that are currently hidden
+ * @return      list of events
+ */
 function postcalendar_admin_listhidden()
 {
     $dom = ZLanguage::getModuleDomain('PostCalendar');
@@ -55,6 +70,11 @@ function postcalendar_admin_listhidden()
     return postcalendar_admin_showlist($args);
 }
 
+/**
+ * @function    postcalendar_admin_listqueued
+ * @description list all events that are awaiting approval
+ * @return      list of events
+ */
 function postcalendar_admin_listqueued()
 {
     $dom = ZLanguage::getModuleDomain('PostCalendar');
@@ -65,6 +85,12 @@ function postcalendar_admin_listqueued()
     return postcalendar_admin_showlist($args);
 }
 
+/**
+ * @function    postcalendar_admin_showlist
+ * @description list events as requested/filtered
+ *              send list to template
+ * @return      showlist template
+ */
 function postcalendar_admin_showlist($args)
 {
     $dom = ZLanguage::getModuleDomain('PostCalendar');
@@ -116,6 +142,11 @@ function postcalendar_admin_showlist($args)
     return $pnRender->fetch('admin/postcalendar_admin_showlist.htm');
 }
 
+/**
+ * @function    postcalendar_admin_adminevents
+ * @description allows admin to revue selected events then take action
+ * @return      adminrevue template
+ */
 function postcalendar_admin_adminevents()
 {
     $dom = ZLanguage::getModuleDomain('PostCalendar');
@@ -177,6 +208,11 @@ function postcalendar_admin_adminevents()
     return $pnRender->fetch("admin/postcalendar_admin_eventrevue.htm");
 }
 
+/**
+ * @function    postcalendar_admin_resetDefaults
+ * @description reset all module variables to default values as defined in pninit.php
+ * @return      status/error ->back to modify config page
+ */
 function postcalendar_admin_resetDefaults()
 {
     $dom = ZLanguage::getModuleDomain('PostCalendar');
@@ -186,7 +222,7 @@ function postcalendar_admin_resetDefaults()
 
     $defaults = pnModFunc('PostCalendar', 'init', 'getdefaults');
     if (!count($defaults)) {
-        return LogUtil::registerError(__('Error! Could not load items.', $dom));
+        return LogUtil::registerError(__('Error! Could not load default values.', $dom));
     }
 
     // delete all the old vars
@@ -202,6 +238,11 @@ function postcalendar_admin_resetDefaults()
     return postcalendar_admin_modifyconfig();
 }
 
+/**
+ * @function    postcalendar_admin_updateconfig
+ * @description sets module variables as requested by admin
+ * @return      status/error ->back to modify config page
+ */
 function postcalendar_admin_updateconfig()
 {
     $dom = ZLanguage::getModuleDomain('PostCalendar');
@@ -211,7 +252,7 @@ function postcalendar_admin_updateconfig()
 
     $defaults = pnModFunc('PostCalendar', 'init', 'getdefaults');
     if (!count($defaults)) {
-        return LogUtil::registerError(__('Error! Could not load items.', $dom));
+        return LogUtil::registerError(__('Error! Could not load default values.', $dom));
     }
 
     $settings = array(
@@ -237,7 +278,6 @@ function postcalendar_admin_updateconfig()
     'pcNotifyAdmin2Admin'     => FormUtil::getPassedValue('pcNotifyAdmin2Admin', 0),
     'pcAllowCatFilter'        => FormUtil::getPassedValue('pcAllowCatFilter', 0),
     );
-    // v4b TS end
 
     // delete all the old vars
     pnModDelVar('PostCalendar');
@@ -252,6 +292,12 @@ function postcalendar_admin_updateconfig()
     return postcalendar_admin_modifyconfig();
 }
 
+/**
+ * @decrecated
+ * @function    postcalendar_admin_categories
+ * @description display list of PostCalendar categories
+ * @return      categories template
+ */
 function postcalendar_admin_categories()
 {
     $dom = ZLanguage::getModuleDomain('PostCalendar');
@@ -267,8 +313,15 @@ function postcalendar_admin_categories()
     return $pnRender->fetch('admin/postcalendar_admin_categories.htm');
 }
 
+/**
+ * @decrecated
+ * @function    postcalendar_admin_categoriesConfirm
+ * @description present review of changes to categories
+ * @return      categoriesconfirm template
+ */
 function postcalendar_admin_categoriesConfirm()
 {
+    $dom = ZLanguage::getModuleDomain('PostCalendar');
     if (!pnSecAuthAction(0, 'PostCalendar::', '::', ACCESS_ADMIN)) {
         return LogUtil::registerPermissionError();
     }
@@ -286,8 +339,7 @@ function postcalendar_admin_categoriesConfirm()
 
     if (is_array($del)) {
         $dels = implode(',', $del);
-        $delText = _PC_DELETE_CATS . $dels . '.';
-        $pnRender->assign('delText', $delText);
+        $pnRender->assign('delText', __('Delete Categories?', $dom) . $dels . '.');
         $pnRender->assign('dels', $dels);
     }
     $pnRender->assign('id', serialize($id));
@@ -302,6 +354,12 @@ function postcalendar_admin_categoriesConfirm()
     return $pnRender->fetch('admin/postcalendar_admin_categoriesconfirm.htm');
 }
 
+/**
+ * @decrecated
+ * @function    postcalendar_admin_categoriesUpdate
+ * @description modify categories (add/delete/edit)
+ * @return      status/error -> return to categories display
+ */
 function postcalendar_admin_categoriesUpdate()
 {
     $dom = ZLanguage::getModuleDomain('PostCalendar');
@@ -357,7 +415,7 @@ function postcalendar_admin_categoriesUpdate()
         $obj['catcolor'] = $modColor[$k];
         $res = DBUtil::updateObject($obj, 'postcalendar_categories', '', 'catid');
         if (!$res) {
-            LogUtil::registerError('_PC_UPDATE_FAILED');
+            LogUtil::registerError(__('Categories update failed.', $dom));
             $action_status = false;
         }
     }
@@ -366,7 +424,7 @@ function postcalendar_admin_categoriesUpdate()
     if (isset($dels) && $dels) {
         $res = DBUtil::deleteObjectsFromKeyArray(array_flip($del), 'postcalendar_categories', 'catid');
         if (!$res) {
-            LogUtil::registerError('_PC_DELETE_FAILED');
+            LogUtil::registerError(__('Category delete failed.', $dom));
             $action_status = false;
         }
     }
@@ -379,15 +437,20 @@ function postcalendar_admin_categoriesUpdate()
         $obj['catcolor'] = $newcolor;
         $res = DBUtil::insertObject($obj, 'postcalendar_categories', false, 'catid');
         if (!$res) {
-            LogUtil::registerError('_PC_INSERT_FAILED');
+            LogUtil::registerError(__('New category creation failed.', $dom));
             $action_status = false;
         }
     }
 
-    if ($action_status) LogUtil::registerStatus('_PC_DONE'); // category updated/deleted/added
+    if ($action_status) LogUtil::registerStatus(__('Category modified as requested.', $dom)); // category updated/deleted/added
     return postcalendar_admin_categories();
 }
 
+/**
+ * @function    postcalendar_admin_manualClearCache
+ * @description clear pnRender Cache
+ * @return      status/error -> return to admin config
+ */
 function postcalendar_admin_manualClearCache()
 {
     $dom = ZLanguage::getModuleDomain('PostCalendar');
@@ -399,10 +462,15 @@ function postcalendar_admin_manualClearCache()
         LogUtil::registerStatus(__('Smarty Cache has been cleared', $dom));
         return postcalendar_admin_modifyconfig();
     }
-    LogUtil::registerStatus(_PC_CACHE_NOTCLEARED);
+    LogUtil::registerError(__('Smarty Cache was not cleared', $dom));
     return postcalendar_admin_modifyconfig();
 }
 
+/**
+ * @function    postcalendar_admin_testSystem
+ * @description show list of information to admin regarding server environment
+ * @return      status/error -> return to admin config
+ */
 function postcalendar_admin_testSystem()
 {
     $dom = ZLanguage::getModuleDomain('PostCalendar');
@@ -432,20 +500,20 @@ function postcalendar_admin_testSystem()
         $pnVersion = pnConfigGetVar('Version_Num');
     }
 
-    array_push($infos, array('Zikula Version', $pnVersion));
-    array_push($infos, array('Sitename', pnConfigGetVar('sitename')));
-    array_push($infos, array('url', pnGetBaseURL()));
-    array_push($infos, array('PHP Version', phpversion()));
+    array_push($infos, array(__('Zikula Version', $dom), $pnVersion));
+    array_push($infos, array(__('Sitename', $dom), pnConfigGetVar('sitename')));
+    array_push($infos, array(__('URL', $dom), pnGetBaseURL()));
+    array_push($infos, array(__('PHP Version', $dom), phpversion()));
     if ((bool) ini_get('safe_mode')) {
-        $safe_mode = "On";
+        $safe_mode = __('ON', $dom);
     } else {
-        $safe_mode = "Off";
+        $safe_mode = __('OFF', $dom);
     }
     array_push($infos, array('PHP safe_mode', $safe_mode));
     if ((bool) ini_get('safe_mode_gid')) {
-        $safe_mode_gid = "On";
+        $safe_mode_gid = __('ON', $dom);
     } else {
-        $safe_mode_gid = "Off";
+        $safe_mode_gid = __('OFF', $dom);
     }
     array_push($infos, array('PHP safe_mode_gid', $safe_mode_gid));
     $base_dir = ini_get('open_basedir');
@@ -454,45 +522,44 @@ function postcalendar_admin_testSystem()
     } else {
         $open_basedir = "NULL";
     }
-    array_push($infos, array('PHP open_basedir', $open_basedir));
+    array_push($infos, array(__('PHP open_basedir', $dom), $open_basedir));
     array_push($infos, array('SAPI', php_sapi_name()));
     array_push($infos, array('OS', php_uname()));
-    array_push($infos, array('WebServer', $__SERVER['SERVER_SOFTWARE']));
-    array_push($infos, array('Module dir', dirname(__FILE__)));
+    array_push($infos, array(__('WebServer', $dom), $__SERVER['SERVER_SOFTWARE']));
+    array_push($infos, array(__('Module dir', $dom), dirname(__FILE__)));
 
     $modversion = array();
     include dirname(__FILE__) . '/pnversion.php';
 
     if ($modversion['version'] != $version) {
-        LogUtil::registerError("new version " . $modversion[version] . " installed but not updated!");
+        LogUtil::registerError(__('New version %s installed but not updated!', $modversion[version], $dom));
     }
-    array_push($infos, array('Module version', $version));
-    array_push($infos, array('smarty version', $tpl->_version));
-    array_push($infos, array('smarty location', SMARTY_DIR));
-    array_push($infos, array('smarty template dir', $tpl->template_dir));
+    array_push($infos, array(__('Module version', $dom), $version));
+    array_push($infos, array(__('Smarty version', $dom), $tpl->_version));
+    array_push($infos, array(__('Smarty location', $dom), SMARTY_DIR));
+    array_push($infos, array(__('Smarty template dir', $dom), $tpl->template_dir));
 
     $info = $tpl->compile_dir;
-    //$error = '';
     if (!file_exists($tpl->compile_dir)) {
-        LogUtil::registerError("compile dir doesn't exist! [" . $tpl->compile_dir . "]");
+        LogUtil::registerError(__('Compile dir doesn\'t exist! [%s]', $tpl->compile_dir, $dom));
     } else {
         // dir exists -> check if it's writeable
         if (!is_writeable($tpl->compile_dir)) {
-            LogUtil::registerError("compile dir not writeable! [" . $tpl->compile_dir . "]");
+            LogUtil::registerError(__('Compile dir not writeable! [%s]', $tpl->compile_dir, $dom));
         }
     }
-    array_push($infos, array('smarty compile dir', $tpl->compile_dir));
+    array_push($infos, array(__('Smarty compile dir', $dom), $tpl->compile_dir));
 
     $info = $tpl->cache_dir;
     if (!file_exists($tpl->cache_dir)) {
-        LogUtil::registerError("cache dir doesn't exist! [" . $tpl->cache_dir . "]");
+        LogUtil::registerError(__('Cache dir doesn\'t exist! [%s]', $tpl->cache_dir, $dom));
     } else {
         // dir exists -> check if it's writeable
         if (!is_writeable($tpl->cache_dir)) {
-            LogUtil::registerError("cache dir not writeable! [" . $tpl->cache_dir . "]");
+            LogUtil::registerError(__('Cache dir not writeable! [%s]', $tpl->cache_dir, $dom));
         }
     }
-    array_push($infos, array('smarty cache dir', $tpl->cache_dir));
+    array_push($infos, array(__('Smarty cache dir', $dom), $tpl->cache_dir));
 
     $tpl->assign('infos', $infos);
     return $tpl->fetch('admin/postcalendar_admin_systeminfo.htm');
