a8688e2aa66145e28bc5e249ca56aa81541b9bdb
Craig Heydenburg <craigh@mac.com>
2010-02-17 01:52:00 +0000

    refs #89. implement class structure for contenttypes (copied from computer code contenttype)

--- PostCalendar/pncontenttypesapi/postcalevent.php
+++ PostCalendar/pncontenttypesapi/postcalevent.php
@@ -7,3 +7,86 @@
  * @copyright   Copyright (c) 2009, Craig Heydenburg, Sound Web Development
  * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License
  */
+
+class postcalendar_contenttypesapi_postcaleventPlugin extends contentTypeBase
+{
+    var $eid; // event id
+
+    function getModule()
+    {
+        return 'postcalendar';
+    }
+    function getName()
+    {
+        return 'postcalevent';
+    }
+    function getTitle()
+    {
+        $dom = ZLanguage::getModuleDomain('PostCalendar');
+        return __('Featured Event', $dom);
+    }
+    function getDescription()
+    {
+        $dom = ZLanguage::getModuleDomain('PostCalendar');
+        return __('Displays one event from PostCalendar.', $dom);
+    }
+
+    function loadData($data)
+    {
+        $this->text = $data['text'];
+    }
+
+    function display()
+    {
+        if (pnModIsHooked('bbcode', 'content')) {
+            $code = '[code]' . $this->text . '[/code]';
+            $code = pnModAPIFunc('bbcode', 'user', 'transform', array('extrainfo' => array($code), 'objectid' => 999));
+            $this->$code = $code[0];
+            return $this->$code;
+        } else {
+            return $this->transformCode($this->text, true);
+        }
+    }
+
+    function displayEditing()
+    {
+        return $this->transformCode($this->text, false); // <pre> does not work in IE 7 with the portal javascript
+    }
+
+    function getDefaultData()
+    {
+        return array('text' => '');
+    }
+
+    function getSearchableText()
+    {
+        return html_entity_decode(strip_tags($this->text));
+    }
+
+    function transformCode($code, $usePre)
+    {
+        $lines = explode("\n", $code);
+        $html = "<div class=\"content-computercode\"><ol class=\"codelisting\">\n";
+
+        for ($i = 1, $cou = count($lines); $i <= $cou; ++$i) {
+            if ($usePre) {
+                $line = empty($lines[$i - 1]) ? ' ' : htmlspecialchars($lines[$i - 1]);
+                $line = '<div><pre>' . $line . '</pre></div>';
+            } else {
+                $line = empty($lines[$i - 1]) ? '&nbsp;' : htmlspecialchars($lines[$i - 1]);
+                $line = str_replace(' ', '&nbsp;', $line);
+                $line = '<div>' . $line . '</div>';
+            }
+            $html .= "<li>$line</li>\n";
+        }
+
+        $html .= "</ol></div>\n";
+
+        return $html;
+    }
+}
+
+function postcalendar_contenttypesapi_postcalevent($args)
+{
+    return new postcalendar_contenttypesapi_postcaleventPlugin($args['data']);
+}
