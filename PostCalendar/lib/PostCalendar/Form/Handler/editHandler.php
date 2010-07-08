20d4897fd9815b126a7d03771df6e0bbf9398b2e
Craig Heydenburg <craigh@mac.com>
2010-07-08 19:18:06 +0000

    fixes in Form handler

--- PostCalendar/lib/PostCalendar/Form/Handler/editHandler.php
+++ PostCalendar/lib/PostCalendar/Form/Handler/editHandler.php
@@ -2,7 +2,7 @@
 /**
  * This is the event handler file
  **/
-class PostCalendar_Form_Handler_editHandler
+class PostCalendar_Form_Handler_EditHandler
 {
     var $eid;
 
@@ -19,23 +19,24 @@ class PostCalendar_Form_Handler_editHandler
 
     function handleCommand(&$render, &$args)
     {
+        $dom = ZLanguage::getModuleDomain('PostCalendar');
         $url = null;
 
         // Fetch event data from DB to confirm event exists
         $event = DBUtil::selectObjectByID('postcalendar_events', $this->eid, 'eid');
         if (count($event) == 0) {
-            return LogUtil::registerError($this->__f('Error! There are no events with ID %s.', $this->eid));
+            return LogUtil::registerError(__f('Error! There are no events with ID %s.', $this->eid, $dom));
         }
 
         if ($args['commandName'] == 'delete') {
             if ((SessionUtil::getVar('uid') != $event['informant']) and (!SecurityUtil::checkPermission('PostCalendar::', '::', ACCESS_ADMIN))) {
-                return LogUtil::registerError($this->__('Sorry! You do not have authorization to delete this event.'));
+                return LogUtil::registerError(__('Sorry! You do not have authorization to delete this event.', $dom));
             }
             $result = DBUtil::deleteObjectByID('postcalendar_events', $this->eid, 'eid');
             if ($result === false) {
-                return LogUtil::registerError($this->__("Error! An 'unidentified error' occurred."));
+                return LogUtil::registerError(__("Error! An 'unidentified error' occurred.", $dom));
             }
-            LogUtil::registerStatus($this->__('Done! The event was deleted.'));
+            LogUtil::registerStatus(__('Done! The event was deleted.', $dom));
             $this->callHooks('item', 'delete', $this->eid, array(
                 'module' => 'PostCalendar'));
 
