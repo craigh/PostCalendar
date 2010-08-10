7c80df49bc4ddde21a5f3e4090108e00ec48168a
Craig Heydenburg <craigh@mac.com>
2010-08-10 01:37:37 +0000

    refs #161. minor case correction

--- PostCalendar/lib/PostCalendar/Handlers.php
+++ PostCalendar/lib/PostCalendar/Handlers.php
@@ -10,7 +10,7 @@ class PostCalendar_Handlers {
     public static function pendingContent(Zikula_Event $event)
     {
         $collection = new Zikula_Collection_Container('PostCalendar');
-        $collection->add(new Zikula_Provider_AggregateItem('submission', __('pending event'), 1, 'Admin', 'listqueued'));
+        $collection->add(new Zikula_Provider_AggregateItem('submission', __('pending event'), 1, 'admin', 'listqueued'));
         $event->getSubject()->add($collection);
     }
 
