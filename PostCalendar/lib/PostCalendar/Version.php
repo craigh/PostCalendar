7c65ab5e3aac1d7b71b6c24a44f11009f0939750
Craig Heydenburg <craigh@mac.com>
2010-08-13 19:02:02 +0000

    update version to final 7.0.0

--- PostCalendar/lib/PostCalendar/Version.php
+++ PostCalendar/lib/PostCalendar/Version.php
@@ -20,7 +20,7 @@ class PostCalendar_Version extends Zikula_Version
         $meta['displayname']    = $this->__('PostCalendar');
         $meta['url']            = $this->__(/*!used in URL - nospaces, no special chars, lcase*/'postcalendar');
         $meta['description']    = $this->__('Calendar for Zikula');
-        $meta['version']        = '7.0.0-dev';
+        $meta['version']        = '7.0.0';
 
         $meta['securityschema'] = array(
             'PostCalendar::Event' => 'Event Title::Event ID',
