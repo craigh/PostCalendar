0930048f21efb3c992975f456b1bb75f23d9b291
Craig Heydenburg <craigh@mac.com>
2012-03-01 16:26:35 -0500

    change Version to 7.1.0

--- src/modules/PostCalendar/lib/PostCalendar/Version.php
+++ src/modules/PostCalendar/lib/PostCalendar/Version.php
@@ -20,13 +20,13 @@ class PostCalendar_Version extends Zikula_AbstractVersion
         $meta['displayname'] = $this->__('PostCalendar');
         $meta['url'] = $this->__(/* !used in URL - nospaces, no special chars, lcase */'postcalendar');
         $meta['description'] = $this->__('Calendar for Zikula');
-        $meta['version'] = '7.0.1';
+        $meta['version'] = '7.1.0';
 
         $meta['securityschema'] = array(
             'PostCalendar::Event' => 'Event Title::Event ID',
             'PostCalendar::' => '::');
         $meta['core_min'] = '1.3.0'; // requires minimum 1.3.0 or later
-        //$meta['core_max'] = '1.3.0'; // doesn't work with versions later than x.x.x
+        $meta['core_max'] = '1.3.99'; // doesn't work with 1.4.0 (yet)
 
         $meta['capabilities'] = array();
         $meta['capabilities'][HookUtil::PROVIDER_CAPABLE] = array('enabled' => true);
