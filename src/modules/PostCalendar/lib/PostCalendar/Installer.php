a78fb15baf26e5190e5ae4e965a3c03b35e2e992
Craig Heydenburg <craigh@mac.com>
2012-03-04 16:31:04 -0500

    improve default event in installer.

--- src/modules/PostCalendar/lib/PostCalendar/Installer.php
+++ src/modules/PostCalendar/lib/PostCalendar/Installer.php
@@ -156,25 +156,14 @@ class PostCalendar_Installer extends Zikula_AbstractInstaller
         $cat = CategoryUtil::getCategoryByPath('/__SYSTEM__/Modules/PostCalendar/Events');
 
         $eventArray = array(
-            'title'          => $this->__('PostCalendar Installed'),
-            'hometext'       => $this->__(':text:On this date, the PostCalendar module was installed. Thank you for trying PostCalendar! This event can be safely deleted if you wish.'),
-            'aid'            => UserUtil::getVar('uid'),
-            'time'           => date("Y-m-d H:i:s"),
-            'informant'      => UserUtil::getVar('uid'),
-            'eventDate'      => date('Y-m-d'),
-            'duration'       => 3600,
-            'recurrtype'     => 0,  //norepeat
-            'recurrspec'     => 'a:5:{s:17:"event_repeat_freq";s:0:"";s:22:"event_repeat_freq_type";s:1:"0";s:19:"event_repeat_on_num";s:1:"1";s:19:"event_repeat_on_day";s:1:"0";s:20:"event_repeat_on_freq";s:0:"";}',
-            'startTime'      => '01:00:00',
-            'alldayevent'    => 1,
-            'location'       => 'a:6:{s:14:"event_location";s:0:"";s:13:"event_street1";s:0:"";s:13:"event_street2";s:0:"";s:10:"event_city";s:0:"";s:11:"event_state";s:0:"";s:12:"event_postal";s:0:"";}',
-            'eventstatus'    => 1,  // approved
-            'sharing'        => 3,  // global
-            'website'        => 'https://github.com/craigh/PostCalendar/wiki',
+            'title' => $this->__('PostCalendar Installed'),
+            'hometext' => ':text:' . $this->__('On this date, the PostCalendar module was installed. Thank you for trying PostCalendar! This event can be safely deleted if you wish.'),
+            'alldayevent' => true,
+            'eventstatus' => PostCalendar_Entity_CalendarEvent::APPROVED,
+            'sharing' => PostCalendar_Entity_CalendarEvent::SHARING_GLOBAL,
+            'website' => 'https://github.com/craigh/PostCalendar/wiki',
             '__CATEGORIES__' => array(
-                'Main' => $cat['id']),
-            '__META__'       => array(
-                'module' => 'PostCalendar'));
+                'Main' => $cat['id']));
 
         try {
             $event = new PostCalendar_Entity_CalendarEvent();
