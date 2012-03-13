10fc61ac68a4bb7cde18e9c7d6f69797c2b5bdb1
Craig Heydenburg <craigh@mac.com>
2012-03-13 16:28:54 -0400

    Modify inheritance a bit. still unworking.

--- src/modules/PostCalendar/lib/PostCalendar/CalenderView/AbstractCalendarViewBase.php
+++ src/modules/PostCalendar/lib/PostCalendar/CalenderView/AbstractCalendarViewBase.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * PostCalendar
  * 
@@ -8,9 +9,9 @@
  * Please see the NOTICE file distributed with this source code for further
  * information regarding copyright and licensing.
  */
-
 abstract class PostCalendar_CalendarView_AbstractCalendarViewBase extends Zikula_AbstractHelper
 {
+
     const SUNDAY_IS_FIRST = 0;
     const MONDAY_IS_FIRST = 1;
     const SATURDAY_IS_FIRST = 6;
@@ -20,62 +21,86 @@ abstract class PostCalendar_CalendarView_AbstractCalendarViewBase extends Zikula
      * @var object 
      */
     private $view;
-    
     protected $template;
     protected $cacheTag;
     protected $cacheId;
     protected $viewtype;
-    
+
     /**
      * Reqested date
      * @var DateTime object 
      */
-    protected $date;
-    protected $last_day;
-
-    protected $username;
+    protected $requestedDate;
+    protected $daysInMonth;
+    protected $userFilter;
     protected $categoryFilter;
     protected $currentUser;
+
     /**
      * Date_Calc instance
      * @var object 
      */
     protected $calc;
-    
+
+    /**
+     * Array of navigation links
+     * @var array 
+     */
+    protected $navigation = array(
+        'previous' => '',
+        'next' => '');
+
+    /**
+     * Array of Information to build Calendar Graph
+     * @var array 
+     */
+    protected $calendarGraph = array(
+        'first' => null,
+        'last' => null,
+        'graph' => null);
+
     /**
      * collection of calendar events
      * @var collection of objects 
      */
     protected $events;
-    
-    public function __construct(Zikula_View $view, $date, $username, $categoryFilter)
+
+    public function __construct(Zikula_View $view, $requestedDate, $userFilter, $categoryFilter)
     {
         $this->domain = ZLanguage::getModuleDomain('PostCalendar');
         $this->view = $view;
         $this->calc = new Date_Calc();
         $this->currentUser = UserUtil::getVar('uid');
-        
-        $this->date = DateTime::createFromFormat('Ymd', $date);
-        $this->last_day = DateUtil::getDaysInMonth($this->date->format('m'), $this->date->format('Y'));
-        
-        $this->username = $username;
+
+        $this->requestedDate = DateTime::createFromFormat('Ymd', $requestedDate);
+        // daysInMonth probably not needed to keep as it is easly calculated
+        $this->daysInMonth = $this->requestedDate->format('t');
+
+        $this->userFilter = $userFilter;
         $this->categoryFilter = $categoryFilter;
+
+        $this->setCacheTag();
         $this->cacheId = $this->cacheTag . '|' . $this->currentUser;
-        
+        $this->view->setCacheId($this->cacheId);
+
         $this->setup();
+        
+        $this->setTemplate();
+
+        return $this;
     }
-    
+
     abstract protected function setup();
-    
+
     abstract protected function setTemplate();
-    
+
     abstract protected function setCacheTag();
-    
+
     abstract public function render();
-    
+
     protected function isCached()
     {
         return (isset($this->template) && $this->view->is_cached($this->template));
     }
-    
+
 }
---
+++ src/modules/PostCalendar/lib/PostCalendar/CalenderView/AbstractDays.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * PostCalendar
+ * 
+ * @license MIT
+ * @copyright   Copyright (c) 2012, Craig Heydenburg, Sound Web Development
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * This class is used as a base class for any CalendarView which displays multiple
+ * days (e.g. Week, Month, etc) 
+ */
+abstract class PostCalendar_CalendarView_AbstractDays extends PostCalendar_CalendarView_AbstractCalendarViewBase
+{
+
+    const SUNDAY_IS_FIRST = 0;
+    const MONDAY_IS_FIRST = 1;
+    const SATURDAY_IS_FIRST = 6;
+    
+    /**
+     * Integer representing the admin-selected value for first day of the week [0-6]
+     * @var integer
+     */
+    protected $firstDayOfWeek;
+
+    /**
+     * long and short are arrays of weekdays in desired order
+     * firstDayOfMonth is the array position of first day [0-6]
+     * lastDayOfMonth is the array position of last day [0-6]
+     * @var array 
+     */
+    protected $dayDisplay = array('long' => array(),
+        'short' => array(),
+        'firstDayOfMonth' => null,
+        'lastDayOfMonth' => null,
+        'dayOfWeek' => null,
+        'colclass' => array(0 => "pcWeekday",
+            1 => "pcWeekday",
+            2 => "pcWeekday",
+            3 => "pcWeekday",
+            4 => "pcWeekday",
+            5 => "pcWeekday",
+            6 => "pcWeekday"));
+
+    function __construct(Zikula_View $view, $requestedDate, $userFilter, $categoryFilter)
+    {
+        parent::__construct($view, $requestedDate, $userFilter, $categoryFilter);
+        $this->firstDayOfWeek = ModUtil::getVar('PostCalendar', 'pcFirstDayOfWeek');
+        $this->setUpDayDisplay();
+        $this->setUpCalendarGraph();
+    }
+
+    /**
+     * This function enables the translation of daynames
+     * and sets up an array of the names in the desired order based on
+     * the selected 'first day of week'
+     * It also created a css class for each day based
+     */
+    protected function setUpDayDisplay()
+    {
+        $arrayPointer = $this->firstDayOfWeek;
+        $shortNames = explode(" ", $this->__(/* !First Letter of each Day of week */'S M T W T F S'));
+        $longNames = explode(" ", $this->__('Sunday Monday Tuesday Wednesday Thursday Friday Saturday'));
+        for ($i = 0; $i < 7; $i++) {
+            if ($arrayPointer >= 7) {
+                $arrayPointer = 0;
+            }
+            $this->dayDisplay['long'][$i] = $longNames[$arrayPointer];
+            $this->dayDisplay['short'][$i] = $shortNames[$arrayPointer];
+            $arrayPointer++;
+        }
+        switch ($this->firstDayOfWeek) {
+            case self::MONDAY_IS_FIRST:
+                $this->dayDisplay['firstDayOfMonth'] = date('w', mktime(0, 0, 0, $this->requestedDate->format('m'), 0, $this->requestedDate->format('Y')));
+                $this->dayDisplay['dayOfWeek'] = date('w', mktime(0, 0, 0, $this->requestedDate->format('m'), $this->requestedDate->format('d') - 1, $this->requestedDate->format('Y')));
+                $this->dayDisplay['lastDayOfMonth'] = date('w', mktime(0, 0, 0, $this->requestedDate->format('m'), $this->requestedDate->format('t') - 1, $this->requestedDate->format('Y')));
+                $this->dayDisplay['colclass'][5] = "pcWeekend";
+                $this->dayDisplay['colclass'][6] = "pcWeekend";
+                break;
+            case self::SATURDAY_IS_FIRST:
+                $this->dayDisplay['firstDayOfMonth'] = date('w', mktime(0, 0, 0, $this->requestedDate->format('m'), 2, $this->requestedDate->format('Y')));
+                $this->dayDisplay['dayOfWeek'] = date('w', mktime(0, 0, 0, $this->requestedDate->format('m'), $this->requestedDate->format('d') + 1, $this->requestedDate->format('Y')));
+                $this->dayDisplay['lastDayOfMonth'] = date('w', mktime(0, 0, 0, $this->requestedDate->format('m'), $this->requestedDate->format('t') + 1, $this->requestedDate->format('Y')));
+                $this->dayDisplay['colclass'][0] = "pcWeekend";
+                $this->dayDisplay['colclass'][1] = "pcWeekend";
+                break;
+            case self::SUNDAY_IS_FIRST:
+            default:
+                $this->dayDisplay['firstDayOfMonth'] = date('w', mktime(0, 0, 0, $this->requestedDate->format('m'), 1, $this->requestedDate->format('Y')));
+                $this->dayDisplay['dayOfWeek'] = date('w', mktime(0, 0, 0, $this->requestedDate->format('m'), $this->requestedDate->format('d'), $this->requestedDate->format('Y')));
+                $this->dayDisplay['lastDayOfMonth'] = date('w', mktime(0, 0, 0, $this->requestedDate->format('m'), $this->requestedDate->format('t'), $this->requestedDate->format('Y')));
+                $this->dayDisplay['colclass'][0] = "pcWeekend";
+                $this->dayDisplay['colclass'][6] = "pcWeekend";
+                break;
+        }
+    }
+
+    protected function setUpCalendarGraph()
+    {
+        
+    }
+}
