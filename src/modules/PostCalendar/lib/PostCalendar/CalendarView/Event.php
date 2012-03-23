d0cd84379b2b913ec266706e8d6fa167d086731c
Craig Heydenburg <craigh@mac.com>
2012-03-23 19:30:29 -0400

    refactor Event CalendarView

--- src/modules/PostCalendar/lib/PostCalendar/CalendarView/Event.php
+++ src/modules/PostCalendar/lib/PostCalendar/CalendarView/Event.php
@@ -11,11 +11,13 @@
  */
 class PostCalendar_CalendarView_Event extends PostCalendar_CalendarView_AbstractCalendarViewBase
 {
+    private $event = array();
 
-    protected $popup = false;
+    private $popup = false;
 
     protected function setCacheTag()
     {
+        // this is unused in this object. It is set to be not null.
         $this->cacheTag = $this->eid;
     }
 
@@ -28,43 +30,43 @@ class PostCalendar_CalendarView_Event extends PostCalendar_CalendarView_Abstract
     {
         $this->viewtype = 'event';
         $this->popup = $this->view->getRequest()->query->get('popup', $this->view->getRequest()->request->get('popup', false));
+        
+        $em = ServiceUtil::getService('doctrine.entitymanager');
+        $event = $em->getRepository('PostCalendar_Entity_CalendarEvent')->find($this->eid)->getOldArray();
+        $this->event = ModUtil::apiFunc('PostCalendar', 'event', 'formateventarrayfordisplay', $event);
     }
 
     public function render()
     {
-        // caching won't help much in this case because security check comes 
-        // after fetch from db, so don't use isCached, just fetch after
-        // normal routine.
+        // caching won't help much in this case because security check comes after
+        // fetch from db, so don't use isCached, just fetch after normal routine.
 
-        $em = ServiceUtil::getService('doctrine.entitymanager');
-        $event = $em->getRepository('PostCalendar_Entity_CalendarEvent')->find($this->eid)->getOldArray();
-        $event = ModUtil::apiFunc('PostCalendar', 'event', 'formateventarrayfordisplay', $event);
         // is event allowed for this user?
-        if (($event['sharing'] == PostCalendar_Entity_CalendarEvent::SHARING_PRIVATE
-                && $event['aid'] != $this->currentUser
+        if (($this->event['sharing'] == PostCalendar_Entity_CalendarEvent::SHARING_PRIVATE
+                && $this->event['aid'] != $this->currentUser
                 && !SecurityUtil::checkPermission('PostCalendar::', '::', ACCESS_ADMIN))
-                || ((!SecurityUtil::checkPermission('PostCalendar::Event', "$event[title]::$event[eid]", ACCESS_OVERVIEW))
-                || (!CategoryUtil::hasCategoryAccess($event['categories'], 'PostCalendar')))) {
+                || ((!SecurityUtil::checkPermission('PostCalendar::Event', "{$this->event['title']}::{$this->event['eid']}", ACCESS_OVERVIEW))
+                || (!CategoryUtil::hasCategoryAccess($this->event['categories'], 'PostCalendar')))) {
             // if event is PRIVATE and user is not assigned event ID (aid) and user is not Admin event should not be seen
             // or if specific event is permission controlled or if Category is denied
             return LogUtil::registerError($this->view->__('You do not have permission to view this event.'));
         }
 
-        // since recurrevents are dynamically calculcated, we need to change the date
+        // since recurrevents are dynamically calculated, we need to change the date
         // to ensure that the correct/current date is being displayed (rather than the
         // date on which the recurring booking was executed).
-        if ($event['recurrtype']) {
-            $event['eventDate'] = $this->requestedDate->format('Ymd');
+        if ($this->event['recurrtype']) {
+            $this->event['eventDate'] = $this->requestedDate->format('Ymd');
         }
         // create and return template
-        $this->view->assign('loaded_event', $event);
+        $this->view->assign('loaded_event', $this->event);
         if ($this->popup) {
             $this->view->assign('popup', true)
                     ->display('event/view.tpl');
             return true;
         } else {
             $edit = ((SecurityUtil::checkPermission('PostCalendar::', '::', ACCESS_ADD)
-                    && (UserUtil::getVar('uid') == $event['aid']))
+                    && (UserUtil::getVar('uid') == $this->event['aid']))
                     || SecurityUtil::checkPermission('PostCalendar::', '::', ACCESS_ADMIN));
             $this->view->assign('EVENT_CAN_EDIT', $edit);
             return $this->view->fetch($this->template);
