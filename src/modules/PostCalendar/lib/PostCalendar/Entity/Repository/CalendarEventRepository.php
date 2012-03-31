662ea281d667e73c7495b75736d4d730db76d62a
Craig Heydenburg <craigh@mac.com>
2012-03-31 09:12:09 -0400

    improve readability of code through improved variable naming, docblocks and formatting

--- src/modules/PostCalendar/lib/PostCalendar/Entity/Repository/CalendarEventRepository.php
+++ src/modules/PostCalendar/lib/PostCalendar/Entity/Repository/CalendarEventRepository.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * PostCalendar
  * 
@@ -8,40 +9,52 @@
  * Please see the NOTICE file distributed with this source code for further
  * information regarding copyright and licensing.
  */
-
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Query;
 use PostCalendar_Entity_CalendarEvent as CalendarEvent;
 
 class PostCalendar_Entity_Repository_CalendarEventRepository extends EntityRepository
 {
-    const FILTER_GLOBAL = -1; // all public events
-    const FILTER_ALL = -2; // all public + my events
-    const FILTER_PRIVATE = -3; // just my private events
     
     /**
-     * get all associated tags for an object
+     * All public events 
+     */
+    const FILTER_GLOBAL = -1;
+    /**
+     * All public + my events 
+     */
+    const FILTER_ALL = -2;
+    /**
+     * Just my private events 
+     */
+    const FILTER_PRIVATE = -3;
+
+    /**
+     * Retrieve filtered count of events
      * This count is not filtered by Permissions
+     * 
+     * @param integer $eventStatus
+     * @param array $categoryFilter
      * 
      * @return Scalar 
      */
-    public function getEventCount($eventStatus = CalendarEvent::APPROVED, $filterCategories = null)
+    public function getEventCount($eventStatus = CalendarEvent::APPROVED, $categoryFilter = null)
     {
         $dql = "SELECT COUNT(DISTINCT a.eid) FROM PostCalendar_Entity_CalendarEvent a JOIN a.categories c ";
         $where = array();
         if ($eventStatus <> CalendarEvent::ALLSTATUS) {
             $where[] = "a.eventstatus = :status ";
         }
-        if (isset($filterCategories) && !empty($filterCategories)) {
+        if (isset($categoryFilter) && !empty($categoryFilter)) {
             // reformat array
-            $categories = array_values($filterCategories);
+            $categories = array_values($categoryFilter);
             // add to dql
             $where[] = "c.category IN (:categories) ";
         }
         if (!empty($where)) {
             $dql .= "WHERE " . implode(' AND ', $where);
         }
-        
+
         $query = $this->_em->createQuery($dql);
         if (isset($categories)) {
             $query->setParameter('categories', $categories);
@@ -51,35 +64,35 @@ class PostCalendar_Entity_Repository_CalendarEventRepository extends EntityRepos
         }
         return $query->getResult(Query::HYDRATE_SINGLE_SCALAR);
     }
-    
+
     /**
-     * Get collection of events for user view
+     * Get filtered collection of events for user view
      * This collection is not filtered by Permissions
      * 
      * @param integer $eventStatus
      * @param string $startDate
      * @param string $endDate
-     * @param integer $username
-     * @param integer $ruserid
-     * @param array $filterCategories
+     * @param integer $userFilter
+     * @param integer $userid
+     * @param array $categoryFilter
      * @param string $search
      * 
      * @return Object Collection 
      */
-    public function getEventCollection($eventStatus, $startDate, $endDate, $username, $ruserid, array $filterCategories, $searchDql)
+    public function getEventCollection($eventStatus, $startDate, $endDate, $userFilter, $userid, array $categoryFilter, $searchDql)
     {
         $startDate->setTime(0, 0);
         $endDate->setTime(23, 59);
         $dql = "SELECT a FROM PostCalendar_Entity_CalendarEvent a JOIN a.categories c " .
-               "WHERE (a.endDate >= ?2 " .
-               "OR (a.endDate = ?3 AND a.recurrtype <> ?4) " .
-               "OR a.eventDate >= ?5) " .
-               "AND a.eventDate <= ?6 ";
+                "WHERE (a.endDate >= ?2 " .
+                "OR (a.endDate = ?3 AND a.recurrtype <> ?4) " .
+                "OR a.eventDate >= ?5) " .
+                "AND a.eventDate <= ?6 ";
 
         if ($eventStatus <> CalendarEvent::ALLSTATUS) {
             $dql .= "AND a.eventstatus = ?1 ";
         }
-        switch ($username) {
+        switch ($userFilter) {
             case self::FILTER_PRIVATE: // show just private events
                 $dql .= "AND a.aid = ?7 " .
                         "AND a.sharing = ?8 ";
@@ -96,14 +109,14 @@ class PostCalendar_Entity_Repository_CalendarEventRepository extends EntityRepos
         if (!empty($searchDql)) {
             $dql .= "AND $searchDql";
         }
-        
-        if (isset($filterCategories) && !empty($filterCategories)) {
+
+        if (isset($categoryFilter) && !empty($categoryFilter)) {
             // reformat array
-            $categories = array_values($filterCategories);
+            $categories = array_values($categoryFilter);
             // add to dql
             $dql .= "AND c.category IN (:categories) ";
         }
-        
+
         // generate query
         $query = $this->_em->createQuery($dql);
 
@@ -117,16 +130,16 @@ class PostCalendar_Entity_Repository_CalendarEventRepository extends EntityRepos
         if ($eventStatus <> CalendarEvent::ALLSTATUS) {
             $query->setParameter(1, $eventStatus);
         }
-        switch ($username) {
+        switch ($userFilter) {
             case self::FILTER_PRIVATE:
                 $query->setParameters(array(
-                    7 => $ruserid,
+                    7 => $userid,
                     8 => CalendarEvent::SHARING_PRIVATE,
                 ));
                 break;
             case self::FILTER_ALL:
                 $query->setParameters(array(
-                    7 => $ruserid,
+                    7 => $userid,
                     8 => CalendarEvent::SHARING_PRIVATE,
                     9 => CalendarEvent::SHARING_GLOBAL,
                 ));
@@ -150,38 +163,37 @@ class PostCalendar_Entity_Repository_CalendarEventRepository extends EntityRepos
             die;
         }
         return $result;
-        
     }
-    
+
     /**
-     * get collection of events for admin view
+     * get filtered collection of events for admin view
      * This collection is not filtered by Permissions
      * 
      * @param integer $eventStatus
-     * @param string $sort
+     * @param string $sortDir
      * @param integer $offset
-     * @param integer $amount
-     * @param array $filterCategories
+     * @param integer $maxResults
+     * @param array $categoryFilter
      * 
      * @return Object Collection 
      */
-    public function getEventList($eventStatus, $sort, $offset, $amount, array $filterCategories)
+    public function getEventList($eventStatus, $sortDir, $offset, $maxResults, array $categoryFilter)
     {
         $dql = "SELECT a FROM PostCalendar_Entity_CalendarEvent a JOIN a.categories c ";
         $where = array();
         if ($eventStatus <> CalendarEvent::ALLSTATUS) {
             $where[] = "a.eventstatus = :status ";
         }
-        if (isset($filterCategories) && !empty($filterCategories)) {
+        if (isset($categoryFilter) && !empty($categoryFilter)) {
             // reformat array
-            $categories = array_values($filterCategories);
+            $categories = array_values($categoryFilter);
             // add to dql
             $where[] = "c.category IN (:categories) ";
         }
         if (!empty($where)) {
             $dql .= "WHERE " . implode(' AND ', $where);
         }
-        $dql .= "ORDER BY $sort ";
+        $dql .= "ORDER BY $sortDir ";
         // generate query
         $query = $this->_em->createQuery($dql);
         if ($eventStatus <> CalendarEvent::ALLSTATUS) {
@@ -193,7 +205,7 @@ class PostCalendar_Entity_Repository_CalendarEventRepository extends EntityRepos
         if ($offset > 0) {
             $query->setFirstResult($offset);
         }
-        $query->setMaxResults($amount);
+        $query->setMaxResults($maxResults);
         try {
             $result = $query->getResult();
         } catch (Exception $e) {
@@ -206,12 +218,19 @@ class PostCalendar_Entity_Repository_CalendarEventRepository extends EntityRepos
         }
         return $result;
     }
-    
+
+    /**
+     * Change status of events
+     * 
+     * @param integer $status
+     * @param array $eids
+     * @return boolean 
+     */
     public function updateEventStatus($status, array $eids)
     {
         $dql = "UPDATE PostCalendar_Entity_CalendarEvent a " .
-               "SET a.eventstatus = :eventstatus " .
-               "WHERE a.eid IN (:eids)";
+                "SET a.eventstatus = :eventstatus " .
+                "WHERE a.eid IN (:eids)";
         $query = $this->_em->createQuery($dql);
         $query->setParameters(array(
             'eventstatus' => $status,
@@ -230,14 +249,20 @@ class PostCalendar_Entity_Repository_CalendarEventRepository extends EntityRepos
         }
         return true;
     }
-    
+
+    /**
+     * delete an array of events by eid
+     * 
+     * @param array $eids
+     * @return boolean 
+     */
     public function deleteEvents(array $eids)
     {
         foreach ($eids as $eid) {
             $event = $this->_em
                     ->getRepository('PostCalendar_Entity_CalendarEvent')
                     ->findOneBy(array(
-                        'eid' => $eid));
+                'eid' => $eid));
             $this->_em->remove($event);
         }
         try {
@@ -248,13 +273,20 @@ class PostCalendar_Entity_Repository_CalendarEventRepository extends EntityRepos
         }
         return true;
     }
-    
+
+    /**
+     * retrieve an event from hook parameters
+     * 
+     * @param Zikula_DisplayHook $hook
+     * @param integer $eid
+     * @return object 
+     */
     public function getHookedEvent(Zikula_DisplayHook $hook, $eid = null)
     {
         $dql = "SELECT a FROM PostCalendar_Entity_CalendarEvent a JOIN a.categories c " .
-               "WHERE a.hooked_modulename = :modulename " .
-               "AND a.hooked_objectid = :objectid " .
-               "AND a.hooked_area = :area ";
+                "WHERE a.hooked_modulename = :modulename " .
+                "AND a.hooked_objectid = :objectid " .
+                "AND a.hooked_area = :area ";
         if (isset($eid)) {
             $dql .= "AND a.eid = :eid ";
         }
@@ -267,7 +299,7 @@ class PostCalendar_Entity_Repository_CalendarEventRepository extends EntityRepos
         if (isset($eid)) {
             $query->setParameter('eid', $eid);
         }
-    
+
         try {
             $result = $query->getResult();
         } catch (Exception $e) {
@@ -280,4 +312,5 @@ class PostCalendar_Entity_Repository_CalendarEventRepository extends EntityRepos
         }
         return isset($result[0]) ? $result[0] : null;
     }
+
 }
