9e96b320689984c44af61590015d8140a6f9338c
Craig Heydenburg <craigh@mac.com>
2012-04-04 15:37:38 -0400

    add javascript file for new event creation

---
+++ src/modules/PostCalendar/javascript/postcalendar-submitform.js
@@ -0,0 +1,58 @@
+jQuery(document).ready(function(){
+    jQuery('#postcalendar_events_alldayevent').click(function(){
+        if (jQuery('#postcalendar_events_alldayevent').is(':checked')) {
+            jQuery('#eventstart_time').hide();
+            jQuery('#eventend_time').hide();
+        } else {
+            jQuery('#eventstart_time').show();
+            jQuery('#eventend_time').show();
+        }
+    });
+    jQuery('#postcalendar_events_repeats').click(function(){
+        if (jQuery('#postcalendar_events_repeats').is(':checked')) {
+            jQuery('#postcalendar_repetitionsettings').show("slow");
+        } else {
+            jQuery('#postcalendar_repetitionsettings').hide("slow");
+        }
+    });
+    jQuery('#postcalendar_events_htmlortext').click(function(){
+        if (jQuery('#postcalendar_events_htmlortext').is(':checked')) {
+            jQuery('#html_warning').show("slow");
+        } else {
+            jQuery('#html_warning').hide("slow");
+        }
+    });
+    jQuery('#postcalendar_events_haslocation').click(function(){
+        if (jQuery('#postcalendar_events_haslocation').is(':checked')) {
+            jQuery('#postcalendar_events_haslocation_display').show("slow");
+        } else {
+            jQuery('#postcalendar_events_haslocation_display').hide("slow");
+        }
+    });
+    jQuery('#postcalendar_events_hascontact').click(function(){
+        if (jQuery('#postcalendar_events_hascontact').is(':checked')) {
+            jQuery('#postcalendar_events_hascontact_display').show("slow");
+        } else {
+            jQuery('#postcalendar_events_hascontact_display').hide("slow");
+        }
+    });
+});
+function updateFields(inputElementObj, dateText)
+{
+    var fieldName = jQuery(inputElementObj).attr('id');
+    var date = new Date(dateText);
+    switch(fieldName)
+    {
+        case 'eventstart_display':
+            jQuery("#eventend_display").datepicker('option', 'minDate', date);
+            break;
+        case 'eventend_display':
+            jQuery("#eventstart_display").datepicker('option', 'maxDate', date);
+            date.setDate(date.getDate()+1);
+            jQuery("#repeat_enddate_display").datepicker('option', 'minDate', date);
+            break;
+        case 'repeat_enddate_display':
+            // do nothing
+            break;
+    }
+}
