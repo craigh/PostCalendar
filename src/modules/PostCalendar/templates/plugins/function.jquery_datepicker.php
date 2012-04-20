d6858e7730b33d33a9203e39499ac3a48711ccbb
Craig Heydenburg <craigh@mac.com>
2012-04-19 22:53:41 -0400

    Add docblocks and much documentation to plugin

--- src/modules/PostCalendar/templates/plugins/function.jquery_datepicker.php
+++ src/modules/PostCalendar/templates/plugins/function.jquery_datepicker.php
@@ -1,39 +1,143 @@
 <?php
 
+/**
+ * Copyright Zikula Foundation 2012 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula_View
+ * @subpackage Template_Plugins
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * Inject the jQuery Datepicker plugin to the template.
+ * @see http://jqueryui.com/demos/datepicker/
+ *
+ * Available attributes:
+ *  - see inline docblocks of each parameter
+ *
+ * Examples:
+ *
+ *  Displays the datepicker with the current date as default:
+ *
+ *  <samp>{jquery_datepicker displayelement='date'}</samp>
+ *
+ * @param array       $params All attributes passed to this function from the template.
+ * @param Zikula_View $view   Reference to the {@link Zikula_View} object.
+ *
+ * @return string the html and javascript required to render the datepicker
+ */
 function smarty_function_jquery_datepicker($params, Zikula_View $view)
 {
+    /**
+     * defaultdate
+     * The initial date selected and displayed (default: now)
+     */
     $defaultDate = (isset($params['defaultdate'])) ? $params['defaultdate'] : new DateTime();
-    $displayElement = (isset($params['displayelement'])) ? $params['displayelement'] : '';
-    $valueStorageElement = (isset($params['valuestorageelement'])) ? $params['valuestorageelement'] : '';
-    $valueStorageFormat_dateTime = (isset($params['valuestorageformat'])) ? $params['valuestorageformat'] : 'Y-m-d';
-    $valueStorageFormat_javascript = (isset($params['valuestorageformat_javascript'])) ? $params['valuestorageformat_javascript'] : str_replace(array('Y', 'm', 'd'), array('yy', 'mm', 'dd'), $valueStorageFormat_dateTime);
+    /**
+     * displayelement
+     * (required) The id text of the html element where the datepicker displays the selection 
+     */
+    $displayElement = (isset($params['displayelement'])) ? $params['displayelement'] : null;
+    /**
+     * displayformat_datetime
+     * (optional) The php Date format used for the display element (default: 'j F Y')
+     */
     $displayFormat_dateTime = (isset($params['displayformat_datetime'])) ? $params['displayformat_datetime'] : 'j F Y';
+    /**
+     * displayformat_javascript
+     * (optional) The javascript date format used for display (should mirror the dateTime version) (default: 'd MM yy')
+     * @see http://docs.jquery.com/UI/Datepicker/formatDate
+     */
     $displayFormat_javascript = (isset($params['displayformat_javascript'])) ? $params['displayformat_javascript'] : 'd MM yy';
+    /**
+     * valuestorageelement
+     * (optional) the id text of the html element where the selected date will be stored (default null)
+     */
+    $valueStorageElement = (isset($params['valuestorageelement'])) ? $params['valuestorageelement'] : null;
+    /**
+     * valuestorageformat
+     * (optional) the php Date format used for the date passed to the Form (default: 'Y-m-d') 
+     */
+    $valueStorageFormat_dateTime = (isset($params['valuestorageformat'])) ? $params['valuestorageformat'] : 'Y-m-d';
+    /**
+     * valuestorageformat_javascript
+     * (optional) the javascript date format used for the storage element (should mirror the dateTime version) (default: 'yy-mm-dd')
+     * @see http://docs.jquery.com/UI/Datepicker/formatDate
+     */
+    $valueStorageFormat_javascript = (isset($params['valuestorageformat_javascript'])) ? $params['valuestorageformat_javascript'] : str_replace(array('Y', 'm', 'd'), array('yy', 'mm', 'dd'), $valueStorageFormat_dateTime);
+    /**
+     * onselectcallback
+     * (optional) javascript to perform onSelect event (default: null) 
+     */
     $onSelectCallback = (isset($params['onselectcallback'])) ? $params['onselectcallback'] : null;
+    /**
+     * readonly
+     * (optional) whether the display field is readonly of active (default: (boolean)true - IS readonly) 
+     */
     $readOnly = (isset($params['readonly'])) ? $params['readonly'] : true;
+    /**
+     * object
+     * (optional) object name for html element names. e.g. name='myObjectName[myVariable]' (default: null) 
+     */
     $object = (isset($params['object'])) ? $params['object'] : null;
+    /**
+     * mindate
+     * (optional) minimum date allowed to be selected in datepicker (default: null - choose any date)
+     */
     $minDate = (isset($params['mindate'])) ? $params['mindate'] : null;
+    /**
+     * maxdate
+     * (optional) maximum date allowed to be selected in datepicker (default: null - choose any date) 
+     */
     $maxDate = (isset($params['maxdate'])) ? $params['maxdate'] : null;
+    /**
+     * autosize
+     * (optional) resize the display field to match the length of the text (default: (string)true) 
+     */
     $autoSize = (isset($params['autosize'])) ? $params['autosize'] : 'true';
+    /**
+     * theme
+     * (optional) which jquery theme to use for this plugin. Uses JQueryUtil::loadTheme() (default: 'base')
+     */
     $jQueryTheme = (isset($params['theme'])) ? $params['theme'] : 'base';
+    /**
+     * lang
+     * (optional) language of datepicker (default: current system language)
+     */
     $lang = (isset($params['lang'])) ? $params['lang'] : ZLanguage::getLanguageCode();
-    
+
+    // check required params
+    if (!isset($displayElement)) {
+        $view->trigger_error(__f('Error! in %1$s: the %2$s parameter must be specified.', array('jquery_datepicker', 'displayelement')));
+        return false;
+    }
     $minDateValue = ($minDate instanceof DateTime) ? $minDate->format($displayFormat_dateTime) : $minDate;
     $maxDateValue = ($maxDate instanceof DateTime) ? $maxDate->format($displayFormat_dateTime) : $maxDate;
-        
+
+    // load requried javascripts
     PageUtil::addVar("javascript", "jquery-ui");
     if (!empty($lang) && ($lang <> 'en')) {
         PageUtil::addVar("javascript", "javascript/jquery-ui/i18n/jquery.ui.datepicker-$lang.js");
     }
     JQueryUtil::loadTheme($jQueryTheme);
-    
+
+    // build the datepicker
     $javascript = "
         jQuery(document).ready(function() {
             jQuery('#$displayElement').datepicker({
-                altField: '#$valueStorageElement',
-                altFormat: '$valueStorageFormat_javascript',
                 dateFormat: '$displayFormat_javascript',
                 defaultDate: '{$defaultDate->format($displayFormat_dateTime)}',";
+    if (isset($valueStorageElement)) {
+        $javascript .="
+                altField: '#$valueStorageElement',
+                altFormat: '$valueStorageFormat_javascript',";
+    }
     if (isset($minDate)) {
         $javascript .= "
                 minDate: '$minDateValue',";
@@ -53,11 +157,13 @@ function smarty_function_jquery_datepicker($params, Zikula_View $view)
     PageUtil::addVar("footer", "<script type='text/javascript'>$javascript</script>");
 
     $readOnlyHtml = ($readOnly) ? " readonly='readonly'" : "";
-    
-    $name = isset($object) ? "{$object}[{$valueStorageElement}]" : $valueStorageElement;
 
-    $html = "<input type='text'{$readOnlyHtml} id='$displayElement' name='$displayElement' value='{$defaultDate->format($displayFormat_dateTime)}' />\n
-        <input type='hidden' id='$valueStorageElement' name='{$name}' value='{$defaultDate->format($valueStorageFormat_dateTime)}' />";
+    $name = isset($object) ? "{$object}[{$displayElement}]" : $displayElement;
+    $html = "<input type='text'{$readOnlyHtml} id='$displayElement' name='$name' value='{$defaultDate->format($displayFormat_dateTime)}' />\n";
+    if (isset($valueStorageElement)) {
+        $name = isset($object) ? "{$object}[{$valueStorageElement}]" : $valueStorageElement;
+        $html .= "<input type='hidden' id='$valueStorageElement' name='$name' value='{$defaultDate->format($valueStorageFormat_dateTime)}' />";
+    }
 
     return $html;
 }
