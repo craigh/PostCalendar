884b7a2cc322d1e9fdcd04572f5c3064e9306e32
Craig Heydenburg <craigh@mac.com>
2012-04-21 09:09:29 -0400

    add docblocs and documentation to date and time pickers

--- src/modules/PostCalendar/templates/plugins/function.jquery_datepicker.php
+++ src/modules/PostCalendar/templates/plugins/function.jquery_datepicker.php
@@ -36,78 +36,93 @@ function smarty_function_jquery_datepicker($params, Zikula_View $view)
 {
     /**
      * defaultdate
+     * php DateTime object
      * The initial date selected and displayed (default: now)
      */
-    $defaultDate = (isset($params['defaultdate'])) ? $params['defaultdate'] : new DateTime();
+    $defaultDate = (isset($params['defaultdate']) && ($params['defaultdate'] instanceof DateTime)) ? $params['defaultdate'] : new DateTime();
     /**
      * displayelement
+     * string (do not include the '#' character)
      * (required) The id text of the html element where the datepicker displays the selection 
      */
     $displayElement = (isset($params['displayelement'])) ? $params['displayelement'] : null;
     /**
      * displayformat_datetime
+     * string
      * (optional) The php Date format used for the display element (default: 'j F Y')
      */
     $displayFormat_dateTime = (isset($params['displayformat_datetime'])) ? $params['displayformat_datetime'] : 'j F Y';
     /**
      * displayformat_javascript
+     * string
      * (optional) The javascript date format used for display (should mirror the dateTime version) (default: 'd MM yy')
      * @see http://docs.jquery.com/UI/Datepicker/formatDate
      */
     $displayFormat_javascript = (isset($params['displayformat_javascript'])) ? $params['displayformat_javascript'] : 'd MM yy';
     /**
      * valuestorageelement
+     * string (do not include the '#' character)
      * (optional) the id text of the html element where the selected date will be stored (default null)
      */
     $valueStorageElement = (isset($params['valuestorageelement'])) ? $params['valuestorageelement'] : null;
     /**
      * valuestorageformat
+     * string
      * (optional) the php Date format used for the date passed to the Form (default: 'Y-m-d') 
      */
     $valueStorageFormat_dateTime = (isset($params['valuestorageformat'])) ? $params['valuestorageformat'] : 'Y-m-d';
     /**
      * valuestorageformat_javascript
-     * (optional) the javascript date format used for the storage element (should mirror the dateTime version) (default: 'yy-mm-dd')
+     * string
+     * (optional) the javascript date format used for the storage element (should mirror the dateTime version) (default: computed from valuestorageformat)
      * @see http://docs.jquery.com/UI/Datepicker/formatDate
      */
     $valueStorageFormat_javascript = (isset($params['valuestorageformat_javascript'])) ? $params['valuestorageformat_javascript'] : str_replace(array('Y', 'm', 'd'), array('yy', 'mm', 'dd'), $valueStorageFormat_dateTime);
     /**
      * onselectcallback
+     * string
      * (optional) javascript to perform onSelect event (default: null) 
      */
     $onSelectCallback = (isset($params['onselectcallback'])) ? $params['onselectcallback'] : null;
     /**
      * readonly
+     * boolean
      * (optional) whether the display field is readonly of active (default: (boolean)true - IS readonly) 
      */
     $readOnly = (isset($params['readonly'])) ? $params['readonly'] : true;
     /**
      * object
+     * string
      * (optional) object name for html element names. e.g. name='myObjectName[myVariable]' (default: null) 
      */
     $object = (isset($params['object'])) ? $params['object'] : null;
     /**
      * mindate
+     * mixed (php DateTime object or string formatted in same manner as display object
      * (optional) minimum date allowed to be selected in datepicker (default: null - choose any date)
      */
     $minDate = (isset($params['mindate'])) ? $params['mindate'] : null;
     /**
      * maxdate
+     * mixed (php DateTime object or string formatted in same manner as display object
      * (optional) maximum date allowed to be selected in datepicker (default: null - choose any date) 
      */
     $maxDate = (isset($params['maxdate'])) ? $params['maxdate'] : null;
     /**
      * autosize
+     * sstring (not boolean!)
      * (optional) resize the display field to match the length of the text (default: (string)true) 
      */
     $autoSize = (isset($params['autosize'])) ? $params['autosize'] : 'true';
     /**
      * theme
+     * string
      * (optional) which jquery theme to use for this plugin. Uses JQueryUtil::loadTheme() (default: 'base')
      */
     $jQueryTheme = (isset($params['theme'])) ? $params['theme'] : 'base';
     /**
      * lang
+     * string
      * (optional) language of datepicker (default: current system language)
      */
     $lang = (isset($params['lang'])) ? $params['lang'] : ZLanguage::getLanguageCode();
@@ -117,10 +132,11 @@ function smarty_function_jquery_datepicker($params, Zikula_View $view)
         $view->trigger_error(__f('Error! in %1$s: the %2$s parameter must be specified.', array('jquery_datepicker', 'displayelement')));
         return false;
     }
+
     $minDateValue = ($minDate instanceof DateTime) ? $minDate->format($displayFormat_dateTime) : $minDate;
     $maxDateValue = ($maxDate instanceof DateTime) ? $maxDate->format($displayFormat_dateTime) : $maxDate;
 
-    // load requried javascripts
+    // load required javascripts
     PageUtil::addVar("javascript", "jquery-ui");
     if (!empty($lang) && ($lang <> 'en')) {
         PageUtil::addVar("javascript", "javascript/jquery-ui/i18n/jquery.ui.datepicker-$lang.js");
--- src/modules/PostCalendar/templates/plugins/function.jquery_timepicker.php
+++ src/modules/PostCalendar/templates/plugins/function.jquery_timepicker.php
@@ -1,19 +1,111 @@
 <?php
 
+/**
+ * Copyright Zikula Foundation 2012 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula_View
+ * @subpackage Template_Plugins
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * Inject a jQuery Timepicker plugin to the template.
+ * @see https://github.com/trentrichardson/jQuery-Timepicker-Addon
+ * 
+ * NOTE: This plugin is NOT configured to integrate the datepicker and timepicker
+ *     together as one. It only displays the timepicker.
+ *
+ * Available attributes:
+ *  - see inline docblocks of each parameter
+ *
+ * Examples:
+ *
+ *  Displays the datepicker with the current time as default:
+ *
+ *  <samp>{jquery_timepicker displayelement='time'}</samp>
+ *
+ * @param array       $params All attributes passed to this function from the template.
+ * @param Zikula_View $view   Reference to the {@link Zikula_View} object.
+ *
+ * @return string the html and javascript required to render the timepicker
+ */
 function smarty_function_jquery_timepicker($params, Zikula_View $view)
 {
+    /**
+     * defaultdate
+     * php DateTime object
+     * The initial datetime selected and displayed (default: now)
+     */
     $defaultDate = (isset($params['defaultdate']) && ($params['defaultdate'] instanceof DateTime)) ? $params['defaultdate'] : new DateTime();
+    /**
+     * displayelement
+     * string (do not include the '#' character)
+     * (required) The id text of the html element where the timepicker displays the selection 
+     */
     $displayElement = (isset($params['displayelement'])) ? $params['displayelement'] : '';
-    $valueStorageElement = (isset($params['valuestorageelement'])) ? $params['valuestorageelement'] : '';
+    /**
+     * valuestorageelement
+     * string (do not include the '#' character)
+     * (optional) the id text of the html element where the selected time will be stored (default null)
+     * note: storage format is HH:MM (in 24 hour format)
+     */
+    $valueStorageElement = (isset($params['valuestorageelement'])) ? $params['valuestorageelement'] : null;
+    /**
+     * readonly
+     * boolean
+     * (optional) whether the display field is readonly of active (default: (boolean)true - IS readonly) 
+     */
     $readOnly = (isset($params['readonly'])) ? $params['readonly'] : true;
+    /**
+     * object
+     * string
+     * (optional) object name for html element names. e.g. name='myObjectName[myVariable]' (default: null) 
+     */
     $object = (isset($params['object'])) ? $params['object'] : true;
+    /**
+     * inlinestyle
+     * string
+     * contents of html style param - useful for setting display:none on load
+     */
     $inlineStyle = (isset($params['inlinestyle'])) ? $params['inlinestyle'] : null;
+    /**
+     * onclosecallback
+     * string
+     * (optional) javascript to perform onClose event (default: null) 
+     */
     $onCloseCallback = (isset($params['onclosecallback'])) ? $params['onclosecallback'] : null;
+    /**
+     * theme
+     * string
+     * (optional) which jquery theme to use for this plugin. Uses JQueryUtil::loadTheme() (default: 'base')
+     */
     $jQueryTheme = (isset($params['theme'])) ? $params['theme'] : 'base';
+    /**
+     * lang
+     * string
+     * (optional) language of datepicker (default: current system language)
+     */
     $lang = (isset($params['lang'])) ? $params['lang'] : ZLanguage::getLanguageCode();
+    /**
+     * use24hour
+     * boolean
+     * (optional) use 24 hour time display or 12 hour am/pm (default: false) 
+     */
     $use24hour = (isset($params['use24hour'])) ? $params['use24hour'] : false;
+    /**
+     * stepminute
+     * integer
+     * (optional) snapto minute interval in minute slider selector (default: 1)
+     */
     $stepMinute = (isset($params['stepminute'])) ? $params['stepminute'] : 1;
-    
+
+    // compute formats
     if ($use24hour) {
         $ap = 'false';
         $jqueryTimeFormat = 'h:mm';
@@ -23,7 +115,8 @@ function smarty_function_jquery_timepicker($params, Zikula_View $view)
         $jqueryTimeFormat = 'h:mm tt';
         $dateTimeFormat = 'g:i a';
     }
-    
+
+    // load required javascripts
     PageUtil::addVar("javascript", "jquery-ui");
     PageUtil::addVar("javascript", "javascript/jQuery-Timepicker-Addon/jquery-ui-timepicker-addon.js");
     if (!empty($lang) && ($lang <> 'en')) {
@@ -32,6 +125,7 @@ function smarty_function_jquery_timepicker($params, Zikula_View $view)
     JQueryUtil::loadTheme($jQueryTheme);
     PageUtil::addVar("stylesheet", "javascript/jQuery-Timepicker-Addon/jquery-ui-timepicker-addon.css");
 
+    // build the timepicker
     $javascript = "
         jQuery(document).ready(function() {
             jQuery('#$displayElement').timepicker({
@@ -58,7 +152,7 @@ function smarty_function_jquery_timepicker($params, Zikula_View $view)
     $inlineStyle = (isset($inlineStyle)) ? " style='$inlineStyle'" : '';
 
     $html = "<input type='text'{$readOnlyHtml}{$inlineStyle} id='$displayElement' name='{$displayElement}' value='{$defaultDate->format($dateTimeFormat)}' />\n";
-    if (!empty($valueStorageElement)) {
+    if (isset($valueStorageElement)) {
         $html .="<input type='hidden' id='$valueStorageElement' name='{$object}[{$valueStorageElement}]' value='{$defaultDate->format('G:i')}' />\n";
     }
 
