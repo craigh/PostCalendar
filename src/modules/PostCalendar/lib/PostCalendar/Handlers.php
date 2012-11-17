0086f8a6b6fd5f7dc271f357999634188e987273
Craig Heydenburg <craigh@mac.com>
2012-11-17 09:33:13 -0500

    fix for pending content handler

--- src/modules/PostCalendar/lib/PostCalendar/Handlers.php
+++ src/modules/PostCalendar/lib/PostCalendar/Handlers.php
@@ -23,7 +23,8 @@ class PostCalendar_Handlers
     public static function pendingContent(Zikula_Event $event)
     {
         $dom = ZLanguage::getModuleDomain('PostCalendar');
-        $count = $this->entityManager->getRepository('PostCalendar_Entity_CalendarEvent')->getEventCount(PostCalendar_Entity_CalendarEvent::QUEUED);
+        $em = ServiceUtil::getService('doctrine.entitymanager');
+        $count = $em->getRepository('PostCalendar_Entity_CalendarEvent')->getEventCount(PostCalendar_Entity_CalendarEvent::QUEUED);
         if ($count > 0) {
             $collection = new Zikula_Collection_Container('PostCalendar');
             $collection->add(new Zikula_Provider_AggregateItem('submission', _n('Calendar event', 'Calendar events', $count, $dom), $count, 'admin', 'listevents'));
